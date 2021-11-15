e02fda3fdc6d7b6df70d71ad1e410e209138438e
Kuni <kkatsuya@chanzuckerberg.com>
2021-11-15 11:08:43 -0500

    Bump version: 2.0.4 → 2.1.0-rc.0

--- cellxgene_schema_cli/setup.py
+++ cellxgene_schema_cli/setup.py
@@ -5,7 +5,7 @@ with open("requirements.txt") as fh:
 
 setup(
     name="cellxgene-schema",
-    version="2.0.4",
+    version="2.1.0-rc.0",
     url="https://github.com/chanzuckerberg/single-cell-curation",
     license="MIT",
     author="Chan Zuckerberg Initiative",
