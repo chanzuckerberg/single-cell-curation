7cfe3cd797afd3d8939638015d5119a311f53936
Emanuele Bezzi <ebezzi@chanzuckerberg.com>
2022-08-10 18:45:15 -0400

    stuff

--- cellxgene_schema_cli/cellxgene_schema/convert.py
+++ cellxgene_schema_cli/cellxgene_schema/convert.py
@@ -6,18 +6,14 @@ def convert(input_file, output_file):
     
     dataset = ad.read_h5ad(input_file)
 
-    # update obs entries to remove clarifying text from assay_ontology_term_id (for testing fixture, not required by 2to3)
-    dataset.obs.loc[dataset.obs["assay_ontology_term_id"] == 'EFO:0009922 (sci-plex)', 'assay_ontology_term_id'] = 'EFO:0009922'
-    dataset.obs["assay_ontology_term_id"].cat.remove_unused_categories(inplace=True)
-
     # Rename ethnicity_ontology_term_id field
     dataset.obs.rename(columns={"ethnicity_ontology_term_id":"self_reported_ethnicity_ontology_term_id"}, inplace=True)
-    # add 'multiethnic' as a self_reported_ethnicity_ontology_term_id category (for testing fixture, not required by 2to3)
-    dataset.obs["self_reported_ethnicity_ontology_term_id"].cat.add_categories("multiethnic", inplace=True)
-    dataset.obs["self_reported_ethnicity_ontology_term_id"][3] = "multiethnic"
 
     # Set schema version to 3.0.0
     dataset.uns["schema_version"] = "3.0.0"
+
+    # Remove X_normalization
+    del dataset.uns["X_normalization"]
 
     dataset.write(output_file)
 
