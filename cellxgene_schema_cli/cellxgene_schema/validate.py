ca0493163141d5acbefa08ef9a895f6f87727786
nayib-jose-gloria <ngloria@chanzuckerberg.com>
2022-09-16 10:48:14 -0400

    chore: update docs and logs to reference Seurat v4 instead of v3

--- cellxgene_schema_cli/cellxgene_schema/validate.py
+++ cellxgene_schema_cli/cellxgene_schema/validate.py
@@ -927,14 +927,14 @@ class Validator:
             if effective_r_array_size > self.schema_def["max_size_for_seurat"]:
                 if is_sparse:
                     self.warnings.append(
-                        f"This dataset cannot be converted to the .rds (Seurat v3) format. "
+                        f"This dataset cannot be converted to the .rds (Seurat v4) format. "
                         f"{effective_r_array_size} nonzero elements in matrix {matrix_name} exceed the "
                         f"limitations in the R dgCMatrix sparse matrix class (2^31 - 1 nonzero "
                         f"elements)."
                     )
                 else:
                     self.warnings.append(
-                        f"This dataset cannot be converted to the .rds (Seurat v3) format. "
+                        f"This dataset cannot be converted to the .rds (Seurat v4) format. "
                         f"{effective_r_array_size} elements in at least one dimension of matrix "
                         f"{matrix_name} exceed the limitations in the R dgCMatrix sparse matrix class "
                         f"(2^31 - 1 nonzero elements)."
