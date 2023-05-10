f583efbab9950cc2dac1c315daee1897f281e9b3
nayib-jose-gloria <ngloria@chanzuckerberg.com>
2023-05-10 10:47:02 -0400

    pin dependency version + use context manager

--- schema_bump_dry_run_scripts/ontologies/ontology_bump_dry_run.py
+++ schema_bump_dry_run_scripts/ontologies/ontology_bump_dry_run.py
@@ -3,6 +3,7 @@
 import gzip
 import json
 import os
+
 import requests
 
 API_URL = {
@@ -15,8 +16,8 @@ API_URL = {
 def dry_run():
     # Load processed ontologies file
     ontologies = "cellxgene_schema_cli/cellxgene_schema/ontology_files/all_ontology.json.gz"
-    with gzip.open(ontologies, "r") as f:
-        onto_map = json.loads(f.read().decode("utf-8"))
+    with gzip.open(ontologies, "rt") as f:
+        onto_map = json.loads(f.read())
 
     # fetch all currently published datasets
     # TODO: include datasets from private collections (requires curator auth, do not upload to GHA)
@@ -35,37 +36,37 @@ def dry_run():
     }
     # cache terms we know are not deprecated to skip processing; init with special-case, non-ontology terms we use
     non_deprecated_term_set = {"multiethnic", "unknown", "na"}
-    f = open("ontologies-curator-report.txt", "w")
-    # for every dataset, check its ontology term metadata to see if any terms are deprecated. If so, report.
-    for dataset in datasets:
-        for ontology_type in ontology_types:
-            for ontology_term in dataset[ontology_type]:
-                ontology_term_id = ontology_term["ontology_term_id"]
-                if ontology_term_id in non_deprecated_term_set:
-                    continue
-                else:
-                    # all_ontologies is indexed by prefix and term ID without suffixes, so we must build the search term
-                    ontology_id_parts = ontology_term_id.split(" ")[0].split(":")
-                    term_prefix = ontology_id_parts[0]
-                    ontology_index_id = f"{term_prefix}:{ontology_id_parts[1]}"
-                    ontology = onto_map[term_prefix][ontology_index_id]
+    with open("ontologies-curator-report.txt", "w") as f:
+        # for every dataset, check its ontology term metadata to see if any terms are deprecated. If so, report.
+        for dataset in datasets:
+            for ontology_type in ontology_types:
+                for ontology_term in dataset[ontology_type]:
+                    ontology_term_id = ontology_term["ontology_term_id"]
+                    if ontology_term_id in non_deprecated_term_set:
+                        continue
+                    else:
+                        # all_ontologies is indexed by ontology prefix and term ID without suffixes,
+                        # so we must parse + build the search term
+                        ontology_id_parts = ontology_term_id.split(" ")[0].split(":")
+                        term_prefix = ontology_id_parts[0]
+                        ontology_index_id = f"{term_prefix}:{ontology_id_parts[1]}"
+                        ontology = onto_map[term_prefix][ontology_index_id]
 
-                if ontology["deprecated"]:
-                    if not "replaced_by" not in ontology:
-                        f.write("ALERT: Requires Manual Curator Intervention\n")
-                    f.write(f"Collection ID: {dataset['collection_id']}\n")
-                    f.write(f"Dataset ID: {dataset['dataset_id']}\n")
-                    f.write(f"Deprecated Term: {ontology_term_id}\n")
-                    if "replaced_by" in ontology:
-                        f.write(f"Replaced By: {ontology['replaced_by']}\n")
-                    if "consider" in ontology:
-                        f.write(f"Consider: {ontology['consider']}\n")
-                    if "comments" in ontology:
-                        f.write(f"Comments: {ontology['comments']}\n")
-                    f.write("\n")
-                else:
-                    non_deprecated_term_set.add(ontology_term_id)
-    f.close()
+                    if ontology["deprecated"]:
+                        if not "replaced_by" not in ontology:
+                            f.write("ALERT: Requires Manual Curator Intervention\n")
+                        f.write(f"Collection ID: {dataset['collection_id']}\n")
+                        f.write(f"Dataset ID: {dataset['dataset_id']}\n")
+                        f.write(f"Deprecated Term: {ontology_term_id}\n")
+                        if "replaced_by" in ontology:
+                            f.write(f"Replaced By: {ontology['replaced_by']}\n")
+                        if "consider" in ontology:
+                            f.write(f"Consider: {ontology['consider']}\n")
+                        if "comments" in ontology:
+                            f.write(f"Comments: {ontology['comments']}\n")
+                        f.write("\n")
+                    else:
+                        non_deprecated_term_set.add(ontology_term_id)
 
 
 if __name__ == "__main__":
