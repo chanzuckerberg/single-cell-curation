ce2b1a771b382aa02315f24fe70a858af17e37a3
nayib-jose-gloria <ngloria@chanzuckerberg.com>
2023-05-30 16:11:16 -0400

    Bump version: 3.3.0 → 3.4.0-rc.0

--- cellxgene_schema_cli/setup.py
+++ cellxgene_schema_cli/setup.py
@@ -5,7 +5,7 @@ with open("requirements.txt") as fh:
 
 setup(
     name="cellxgene-schema",
-    version="3.3.0",
+    version="3.4.0-rc.0",
     url="https://github.com/chanzuckerberg/single-cell-curation",
     license="MIT",
     author="Chan Zuckerberg Initiative",
