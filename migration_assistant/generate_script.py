56ec914748ba0e417e2d28354f8b8d1db730dc8b
Trent Smith <trent.smith@chanzuckerberg.com>
2023-06-05 10:43:48 -0700

    remove schema version

--- migration_assistant/generate_script.py
+++ migration_assistant/generate_script.py
@@ -1,17 +1,10 @@
 import os
-import subprocess
 from typing import List
 
 from jinja2 import Template
 
 file_path = os.path.dirname(os.path.realpath(__file__))
 target_file = os.path.join(file_path, "../cellxgene_schema_cli/cellxgene_schema/migrate.py")
-
-
-def get_current_version() -> str:
-    version = subprocess.run(["make", "show-current-version"], capture_output=True, text=True).stdout
-    version = version.strip().split("\n")[-1]
-    return version
 
 
 def get_template() -> str:
@@ -22,7 +15,7 @@ def get_template() -> str:
 
 
 def generate_script(template: Template, schema_version: str, ontology_term_map: dict, gencode_term_map: dict):
-    output = template.render(schema_version=schema_version, ontology_term_map=ontology_term_map, gencode_term_map=None)
+    output = template.render(ontology_term_map=ontology_term_map, gencode_term_map=None)
 
     # Overwrite the existing migrate.py file
     with open(target_file, "w") as fp:
@@ -50,10 +43,9 @@ def get_genecode_term_map() -> List[str]:
 
 def main():
     template = get_template()
-    schema_version = get_current_version()
     ontology_term_map = get_ontology_term_map()
     gencode_term_map = get_genecode_term_map()
-    generate_script(template, schema_version, ontology_term_map, gencode_term_map)
+    generate_script(template, ontology_term_map, gencode_term_map)
 
 
 if __name__ == "__main__":
