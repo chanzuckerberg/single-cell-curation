3850cc3728551cda1d8050f87cefc189bdc7dd09
Nayib Gloria <55710092+nayib-jose-gloria@users.noreply.github.com>
2023-06-09 14:45:08 -0400

    feat: consolidate post-reference file bump actions into one workflow + populate automated replaced by map in jinja template (#473)
    * feat: consolidate post-reference file bump actions into one workflow + populate automated replaced by map in jinja template

--- scripts/migration_assistant/tests/test_convert_template.py
+++ scripts/migration_assistant/tests/test_convert_template.py
@@ -17,7 +17,6 @@ def template():
 # Define the test cases
 def test_generate_script(template, tmpdir):
     # Prepare the input data
-    schema_version = "1.0.0"
     ontology_term_map = {
         "assay": {},
         "cell_type": {},
@@ -32,7 +31,7 @@ def test_generate_script(template, tmpdir):
     mock_target_file = tmpdir + "/migrate.py"
     with mock.patch("scripts.migration_assistant.generate_script.target_file", mock_target_file):
         # Execute the function
-        generate_script(get_template(), schema_version, ontology_term_map, gencode_term_map)
+        generate_script(get_template(), ontology_term_map, gencode_term_map)
 
     # Verify the output
     expected_output = """
@@ -98,9 +97,99 @@ def migrate(input_file, output_file, collection_id, dataset_id):
     # No Changes
 
     dataset.write(output_file, compression="gzip")"""
-    mock.patch("migration_assistant.generate_script.get_current_version", return_value=expected_output)
 
     with open(os.path.join(mock_target_file), "r") as fp:
         actual_output = fp.read().strip()
 
     assert actual_output == expected_output[1:]
+
+
+def test_generate_script__with_automated_replaced_by_map(template, tmpdir):
+    ontology_term_map = {
+        "assay": {"EFO:0000002": "EFO:0000001"},
+        "cell_type": {"CL:0000002": "CL:0000001", "CL:0000004": "CL:0000003"},
+        "development_stage": {},
+        "disease": {},
+        "organism": {},
+        "self_reported_ethnicity": {},
+        "sex": {},
+        "tissue": {},
+    }
+    gencode_term_map = []
+    mock_target_file = tmpdir + "/migrate.py"
+    with mock.patch("scripts.migration_assistant.generate_script.target_file", mock_target_file):
+        # Execute the function
+        generate_script(get_template(), ontology_term_map, gencode_term_map)
+
+        # Verify the output
+        expected_output = """
+import anndata as ad
+
+from . import utils
+
+
+def migrate(input_file, output_file, collection_id, dataset_id):
+    print(f"Converting {input_file} into {output_file}")
+
+    dataset = ad.read_h5ad(input_file)
+
+    # ONTOLOGY TERMS TO UPDATE ACROSS ALL DATASETS IN CORPUS
+    # Initialization is AUTOMATED for newly deprecated terms that have 'Replaced By' terms in their ontology files
+
+    # Curators should review the monthly 'Curator Report' and add deprecated term replacements to corresponding map if
+    # 'Replaced By' is not available for a deprecated term.
+
+    # If Curators have non-deprecated term changes to apply to all datasets in the corpus where applicable,
+    # add them here.
+    ontology_term_maps = {
+        "assay": {
+            "EFO:0000002": "EFO:0000001", # AUTOMATED
+        },
+        "cell_type": {
+            "CL:0000002": "CL:0000001", # AUTOMATED
+            "CL:0000004": "CL:0000003", # AUTOMATED
+        },
+        "development_stage": {
+        },
+        "disease": {
+        },
+        "organism": {
+        },
+        "self_reported_ethnicity": {
+        },
+        "sex": {
+        },
+        "tissue": {
+        },
+    }
+
+    # AUTOMATED, DO NOT CHANGE
+    for ontology_name, deprecated_term_map in ontology_term_maps.items():
+        utils.replace_ontology_term(dataset.obs, ontology_name, deprecated_term_map)
+
+    # CURATOR-DEFINED, DATASET-SPECIFIC UPDATES
+    # Use the template below to define dataset and collection specific ontology changes. Will only apply to dataset
+    # if it matches a condition.
+    # If no such changes are needed, leave blank
+    # Examples:
+    # if dataset_id == "<dataset_1_id>":
+    #   <no further logic necessary>
+    #   utils.replace_ontology_term(df, <ontology_name>, {"term_to_replace": "replacement_term", ...})
+    # elif dataset_id == "<dataset_2_id>":
+    #   <custom transformation logic beyond scope of util functions>
+    # elif collection_id == "<collection_1_id>":
+    #   <no further logic necessary>
+    #   utils.replace_ontology_term(df, <ontology_name>, {"term_to_replace": "replacement_term", ...})
+    # elif collection_id == "<collection_2_id>":
+    #   <custom transformation logic beyond scope of replace_ontology_term>
+    # ...
+
+    # AUTOMATED, DO NOT CHANGE -- IF GENCODE UPDATED, DEPRECATED FEATURE FILTERING ALGORITHM WILL GO HERE.
+    # No Changes
+
+    dataset.write(output_file, compression="gzip")"""
+
+        with open(os.path.join(mock_target_file), "r") as fp:
+            actual_output = fp.read().strip()
+
+        assert actual_output == expected_output[1:]
