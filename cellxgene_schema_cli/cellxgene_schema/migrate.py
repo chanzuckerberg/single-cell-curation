6078f6f50827b9a7b05e5d1f271c519db3bcb81e
jahilton <jahilton@stanford.edu>
2023-10-24 16:09:02 -0700

    format changes

--- cellxgene_schema_cli/cellxgene_schema/migrate.py
+++ cellxgene_schema_cli/cellxgene_schema/migrate.py
@@ -55,40 +55,55 @@ def migrate(input_file, output_file, collection_id, dataset_id):
         del dataset.uns["schema_version"]
 
     if collection_id == "ced320a1-29f3-47c1-a735-513c7084d508":
-        utils.replace_ontology_term(dataset.obs, "self_reported_ethnicity",
-            {"HANCESTRO:0487": "HANCESTRO:0598", "HANCESTRO:0496": "HANCESTRO:0597"})
+        utils.replace_ontology_term(
+            dataset.obs,
+            "self_reported_ethnicity",
+            {"HANCESTRO:0487": "HANCESTRO:0598", "HANCESTRO:0496": "HANCESTRO:0597"}
+        )
 
     if dataset.uns["title"] == "adipose from neck - all nuclei":
-        utils.map_ontology_term(dataset.obs, "self_reported_ethnicity", "donor_id",
-            {"CREEK003": "HANCESTRO:0005,HANCESTRO:0013"})
+        utils.map_ontology_term(
+            dataset.obs, "self_reported_ethnicity", "donor_id",{"CREEK003": "HANCESTRO:0005,HANCESTRO:0013"}
+        )
 
     if collection_id == "1ca90a2d-2943-483d-b678-b809bf464c30":
-        utils.map_ontology_term(dataset.obs, "self_reported_ethnicity", "donor_id",
-            {"H20.33.018": "HANCESTRO:0013,HANCESTRO:0014",
-            "H20.33.034": "HANCESTRO:0005",
-            "H21.33.037": "HANCESTRO:0005"})
+        utils.map_ontology_term(
+            dataset.obs,
+            "self_reported_ethnicity", "donor_id",
+            {
+                "H20.33.018": "HANCESTRO:0013,HANCESTRO:0014",
+                "H20.33.034": "HANCESTRO:0005",
+                "H21.33.037": "HANCESTRO:0005"
+            }
+        )
 
     if collection_id == "6f6d381a-7701-4781-935c-db10d30de293":
         new_eth = "HANCESTRO:0005,HANCESTRO:0008"
         study = "homosapiens_None_2023_None_sikkemalisa_"
         donor = "_d10_1101_2022_03_10_4837472020-3173-NC004"
         if dataset_id == "066943a2-fdac-4b29-b348-40cede398e4e":
-            utils.map_ontology_term(dataset.obs, "self_reported_ethnicity", "donor_id",
-                {study + "001" + donor: new_eth})
+            utils.map_ontology_term(
+                dataset.obs, "self_reported_ethnicity", "donor_id",{study + "001" + donor: new_eth}
+            )
         elif dataset_id == "9f222629-9e39-47d0-b83f-e08d610c7479":
-            utils.map_ontology_term(dataset.obs, "self_reported_ethnicity", "donor_id",
-                {study + "002" + donor: new_eth})
+            utils.map_ontology_term(
+                dataset.obs, "self_reported_ethnicity", "donor_id",{study + "002" + donor: new_eth}
+            )
 
     if collection_id == "7d7cabfd-1d1f-40af-96b7-26a0825a306d":
-        donors_to_update = ["Rep_C_1012","Rep_C_1017","Rep_C_1033","Rep_C_1036","Rep_C_1037",
-            "Rep_C_1039","Rep_C_1050","Rep_C_1053","Rep_C_1055","Rep_C_1059","Rep_C_1060",
-            "Rep_C_1064","Rep_C_1076","Rep_C_1078","Rep_C_1094","Rep_C_1095","Rep_C_1107",
-            "Rep_C_1143","Rep_C_1151","Rep_C_1154","Rep_C_1161"]
-        update_map = {d:"HANCESTRO:0014" for d in donors_to_update}
+        donors_to_update = [
+            "Rep_C_1012", "Rep_C_1017", "Rep_C_1033",
+            "Rep_C_1036", "Rep_C_1037", "Rep_C_1039",
+            "Rep_C_1050", "Rep_C_1053", "Rep_C_1055",
+            "Rep_C_1059", "Rep_C_1060", "Rep_C_1064",
+            "Rep_C_1076", "Rep_C_1078", "Rep_C_1094",
+            "Rep_C_1095", "Rep_C_1107", "Rep_C_1143",
+            "Rep_C_1151", "Rep_C_1154", "Rep_C_1161"
+        ]
+        update_map = {d: "HANCESTRO:0014" for d in donors_to_update}
         utils.map_ontology_term(dataset.obs, "self_reported_ethnicity", "donor_id", update_map)
 
-    if collection_id == "4d74781b-8186-4c9a-b659-ff4dc4601d91":
-        if dataset_id == "b07fb54c-d7ad-4995-8bb0-8f3d8611cabe":
+    if collection_id == "4d74781b-8186-4c9a-b659-ff4dc4601d91" and dataset_id == "b07fb54c-d7ad-4995-8bb0-8f3d8611cabe":
             utils.replace_ontology_term(dataset.obs, "cell_type", {"CL:0000234": "CL:0000113"})
 
     if "multiethnic" in dataset.obs["self_reported_ethnicity"].unique():
