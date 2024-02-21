7da68d9f0d84fc7bc6f90ea60d42dd2d362c6b5a
Joyce Yan <5653616+joyceyan@users.noreply.github.com>
2024-02-21 11:59:26 -0800

    feat: bump to schema 5.0 RC (#781)

--- cellxgene_schema_cli/tests/test_schema.py
+++ cellxgene_schema_cli/tests/test_schema.py
@@ -4,4 +4,4 @@ from cellxgene_schema import schema
 
 def test_get_current_schema_version():
     assert semver.Version.is_valid(schema.get_current_schema_version())
-    assert schema.get_current_schema_version() == "4.1.0"
+    assert schema.get_current_schema_version() == "5.0.0"
