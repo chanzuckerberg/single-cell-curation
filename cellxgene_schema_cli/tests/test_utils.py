abcd8e835a76b94b022272aa4af4223c6b1d0d35
Joyce Yan <5653616+joyceyan@users.noreply.github.com>
2024-02-21 16:08:32 -0800

    add tests to test_utils

--- cellxgene_schema_cli/tests/test_utils.py
+++ cellxgene_schema_cli/tests/test_utils.py
@@ -7,6 +7,7 @@ from cellxgene_schema.utils import (
     get_hash_digest_column,
     map_ontology_term,
     read_h5ad,
+    remap_deprecated_features,
     remove_deprecated_features,
     replace_ontology_term,
 )
@@ -27,6 +28,11 @@ def adata_without_raw():
 @pytest.fixture
 def deprecated_features():
     return ["ERCC-00002", "ENSG00000127603"]
+
+
+@pytest.fixture
+def remapped_features():
+    return {"ENSMUSG00000059552": "ENSMUSG00000059552_NEW"}
 
 
 @pytest.fixture
@@ -54,6 +60,39 @@ def test_remove_deprecated_features__without_raw(adata_without_raw, deprecated_f
 
     # Check if the deprecated features are removed
     assert result.var_names.tolist() == ["ENSMUSG00000059552", "ENSSASG00005000004"]
+    assert result.raw is None
+
+
+def test_remap_deprecated_features__with_raw(adata_with_raw, remapped_features):
+    # Call the function under test
+    result = remap_deprecated_features(adata=adata_with_raw, remapped_terms=remapped_features)
+
+    # Check if the deprecated features are replaced
+    assert result.var_names.tolist() == [
+        "ERCC-00002",
+        "ENSG00000127603",
+        "ENSMUSG00000059552_NEW",
+        "ENSSASG00005000004",
+    ]
+    assert result.raw.var_names.tolist() == [
+        "ERCC-00002",
+        "ENSG00000127603",
+        "ENSMUSG00000059552_NEW",
+        "ENSSASG00005000004",
+    ]
+
+
+def test_remap_deprecated_features__without_raw(adata_without_raw, remapped_features):
+    # Call the function under test
+    result = remap_deprecated_features(adata=adata_without_raw, remapped_terms=remapped_features)
+
+    # Check if the deprecated features are replaced
+    assert result.var_names.tolist() == [
+        "ERCC-00002",
+        "ENSG00000127603",
+        "ENSMUSG00000059552_NEW",
+        "ENSSASG00005000004",
+    ]
     assert result.raw is None
 
 
