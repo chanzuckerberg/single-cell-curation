8bd86b54a0220a696c981960ec3cf1453575e5e6
Bento007 <trent.smith007@gmail.com>
2024-05-02 14:39:11 -0700

    undo random formatting changes

--- cellxgene_schema_cli/cellxgene_schema/validate.py
+++ cellxgene_schema_cli/cellxgene_schema/validate.py
@@ -1301,8 +1301,7 @@ class Validator:
             for column in adata_component.columns:
                 if column in component_columns:
                     raise ValueError(
-                        f"Duplicate column name '{column}' detected in 'adata.{df_component}' DataFrame. All "
-                        f"DataFrame column names must be unique."
+                        f"Duplicate column name '{column}' detected in 'adata.{df_component}' DataFrame. All DataFrame column names must be unique."
                     )
                 component_columns.add(column)
 
@@ -1433,8 +1432,7 @@ class Validator:
         # library_id is required if assay is Visium and is_single is True.
         if len(library_ids) == 0:
             self.errors.append(
-                "uns['spatial'] must contain at least one key representing the library_id when obs["
-                "'assay_ontology_term_id'] "
+                "uns['spatial'] must contain at least one key representing the library_id when obs['assay_ontology_term_id'] "
                 "'EFO:0010961' (Visium Spatial Gene Expression) and uns['spatial']['is_single'] is True."
             )
             # Exit as library_id is missing.
@@ -1504,10 +1502,8 @@ class Validator:
                 spot_diameter_fullres = uns_scalefactors["spot_diameter_fullres"]
                 if not isinstance(spot_diameter_fullres, float):
                     self.errors.append(
-                        "uns['spatial'][library_id]['scalefactors']['spot_diameter_fullres'] must be of type float, "
-                        "it is "
-                        f"{type(spot_diameter_fullres)}. This must be the value of the spot_diameter_fullres field "
-                        f"from scalefactors_json.json"
+                        "uns['spatial'][library_id]['scalefactors']['spot_diameter_fullres'] must be of type float, it is "
+                        f"{type(spot_diameter_fullres)}. This must be the value of the spot_diameter_fullres field from scalefactors_json.json"
                     )
 
             # tissue_hires_scalef is required.
@@ -1520,10 +1516,8 @@ class Validator:
                 tissue_hires_scalef = uns_scalefactors["tissue_hires_scalef"]
                 if not isinstance(tissue_hires_scalef, float):
                     self.errors.append(
-                        "uns['spatial'][library_id]['scalefactors']['tissue_hires_scalef'] must be of type float, "
-                        "it is "
-                        f"{type(tissue_hires_scalef)}. This must be the value of the tissue_hires_scalef field from "
-                        f"scalefactors_json.json"
+                        "uns['spatial'][library_id]['scalefactors']['tissue_hires_scalef'] must be of type float, it is "
+                        f"{type(tissue_hires_scalef)}. This must be the value of the tissue_hires_scalef field from scalefactors_json.json"
                     )
 
     def _has_no_extra_keys(self, dictionary: dict, allowed_keys: List[str]) -> bool:
