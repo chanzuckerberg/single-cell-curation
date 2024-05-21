a93983726617f47b191af3ced19dc3b34b0f8af9
Bento007 <trent.smith007@gmail.com>
2024-05-21 15:42:11 -0700

    format

--- cellxgene_schema_cli/cellxgene_schema/migrate.py
+++ cellxgene_schema_cli/cellxgene_schema/migrate.py
@@ -1,4 +1,5 @@
 import anndata as ad
+import numpy as np
 
 from . import utils
 
@@ -66,25 +67,26 @@ def migrate(input_file, output_file, collection_id, dataset_id):
     #   <custom transformation logic beyond scope of replace_ontology_term>
     # ...
 
-    #https://github.com/chanzuckerberg/single-cell-curation/issues/825
+    # https://github.com/chanzuckerberg/single-cell-curation/issues/825
     if collection_id == "0aab20b3-c30c-4606-bd2e-d20dae739c45":
         utils.replace_ontology_term(dataset.obs, "disease", {"MONDO:0060782": "MONDO:0100542"})
 
-    if 'X_spatial' in dataset.obsm_keys():
-        del dataset.obsm['X_spatial']
+    if "X_spatial" in dataset.obsm_keys():
+        del dataset.obsm["X_spatial"]
 
-    if dataset.uns.get('default_embedding') == 'X_spatial':
-        dataset.uns['default_embedding'] = 'spatial'
+    if dataset.uns.get("default_embedding") == "X_spatial":
+        dataset.uns["default_embedding"] = "spatial"
 
-    if dataset.obs['assay_ontology_term_id'].iloc[0] == 'EFO:0010961' \
-    and dataset.uns['spatial']['is_single']:
-        library_id = [k for k in dataset.uns['spatial'].keys() if k != 'is_single'][0]
-        for r in ['hires','fullres']:
-            if r in dataset.uns['spatial'][library_id]['images']:
-                if dataset.uns['spatial'][library_id]['images'][r].dtype == 'float32':
-                    float_array = dataset.uns['spatial'][library_id]['images'][r]
-                    int_array = (float_array * 255).astype(np.uint8)
-                    dataset.uns['spatial'][library_id]['images'][r] = int_array
+    if dataset.obs["assay_ontology_term_id"].iloc[0] == "EFO:0010961" and dataset.uns["spatial"]["is_single"]:
+        library_id = [k for k in dataset.uns["spatial"] if k != "is_single"][0]
+        for r in ["hires", "fullres"]:
+            if (
+                r in dataset.uns["spatial"][library_id]["images"]
+                and dataset.uns["spatial"][library_id]["images"][r].dtype == "float32"
+            ):
+                float_array = dataset.uns["spatial"][library_id]["images"][r]
+                int_array = (float_array * 255).astype(np.uint8)
+                dataset.uns["spatial"][library_id]["images"][r] = int_array
 
     if GENCODE_MAPPER:
         dataset = utils.remap_deprecated_features(adata=dataset, remapped_features=GENCODE_MAPPER)
