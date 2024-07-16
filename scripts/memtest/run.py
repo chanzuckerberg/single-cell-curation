846b5873d0e823e3e6808083e4294835e17edcf3
Bento007 <trent.smith007@gmail.com>
2024-07-16 09:20:04 -0700

    load to a temp file

--- scripts/memtest/run.py
+++ scripts/memtest/run.py
@@ -2,10 +2,10 @@ import tempfile
 
 from cellxgene_schema.validate import validate
 
-H5AD_FILE = "/data/unlabeled.h5ad"
+H5AD_FILE = "/data/dataset_34gb_unlabeled.h5ad"
 
 
 if __name__ == "__main__":
     with tempfile.TemporaryDirectory() as tmpdirname:
-        # tmp_file = "/".join([tmpdirname, "/data/labeled.h5ad"])
-        validate(H5AD_FILE, "/data/labeled.h5ad")
+        tmp_file = "/".join([tmpdirname, "/data/labeled.h5ad"])
+        validate(H5AD_FILE, tmp_file)
