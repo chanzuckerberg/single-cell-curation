aa788dea25ec25334e9bfa267b1374cdc78c7452
Evan Molinelli <emolinelli@Evan-CZI-Laptop.local>
2025-03-10 10:30:44 -0400

    lint checks

--- scripts/schema_bump_dry_run_genes/gene_bump_dry_run.py
+++ scripts/schema_bump_dry_run_genes/gene_bump_dry_run.py
@@ -201,7 +201,7 @@ def generate_deprecated_public(base_url: str, diff_map: Dict) -> Dict:  # type: 
             public_deprecated, is_deprecated_genes_found = compare_genes(dataset, diff_map, public_deprecated)
             if is_deprecated_genes_found:
                 run_reporter.public_deprecated_datasets += 1
-        except Exception as e:
+        except Exception:
             run_reporter.public_errored_datasets += 1
             logger.error(
                 f"Error processing public dataset {dataset['dataset_id']}, in collection {dataset['collection_id']}: "
@@ -236,9 +236,9 @@ def generate_deprecated_private(base_url: str, diff_map: Dict) -> Tuple[Dict, Li
                 if revision_of and revision_of not in non_auto_migrated:
                     non_auto_migrated.add(revision_of)
                     private_deprecated[dataset["collection_id"]]["revision_of"] = revision_of
-        except Exception as e:
+        except Exception:
             run_reporter.private_errored_datasets += 1
-            logger.error(f"Error processing private dataset")
+            logger.error("Error processing private dataset")
     for collection in private_deprecated.values():
         collection["dataset_groups"] = list(collection["dataset_groups"].values())
     return private_deprecated, non_auto_migrated  # type: ignore
