d79199c4bae2ccf184f0fa2b838daf45236f8229
Jim Chaffer <77802611+Jchaffer787@users.noreply.github.com>
2025-03-12 10:35:30 -0700

    Update migrate.py
    Adding Public and Private specific dataset updates, as well as CSR matrix checking according to single-cell-curation issue 1023.

--- cellxgene_schema_cli/cellxgene_schema/migrate.py
+++ cellxgene_schema_cli/cellxgene_schema/migrate.py
@@ -1,26 +1,18 @@
 import anndata as ad
-import dask
-
+import pandas as pd
 from . import utils
+import dask
 
 # fmt: off
 # ONTOLOGY TERMS TO UPDATE ACROSS ALL DATASETS IN CORPUS
-# Initialization is AUTOMATED for newly deprecated terms that have 'Replaced By' terms in their ontology files
-
-# Curators should review the monthly 'Curator Report' and add deprecated term replacements to corresponding map if
-# 'Replaced By' is not available for a deprecated term.
-
-# If Curators have non-deprecated term changes to apply to all datasets in the corpus where applicable,
-# add them here.
 ONTOLOGY_TERM_MAPS = {
     "assay": {
+        "EFO:0010961" : "EFO:0022857", # AUTOMATED VISIUM TERM UPDATE
     },
-    "cell_type": {
+    "cell_type": { 
         "CL:0000402": "CL:0000099", # AUTOMATED
         "CL:0010003": "CL:0000322", # AUTOMATED
         "CL:0000555": "CL:4023161", # AUTOMATED
-    },
-    "development_stage": {
     },
     "disease": {
     },
@@ -35,11 +27,9 @@ ONTOLOGY_TERM_MAPS = {
     },
 }
 
-DEPRECATED_FEATURE_IDS = [
-]
+df = pd.read_csv('migrate_files/non_csr_list.csv')
+NON_CSR_DATASETS = df['dataset_id'].to_list()
 
-# Dictionary for CURATOR-DEFINED remapping of deprecated feature IDs, if any, to new feature IDs.
-GENCODE_MAPPER = {}
 # fmt: on
 
 # Dictionary for CURATOR-DEFINED remapping of deprecated feature IDs, if any, to new feature IDs.
@@ -55,29 +45,104 @@ def migrate(input_file, output_file, collection_id, dataset_id):
     for ontology_name, deprecated_term_map in ONTOLOGY_TERM_MAPS.items():
         utils.replace_ontology_term(dataset.obs, ontology_name, deprecated_term_map)
 
-    # CURATOR-DEFINED, DATASET-SPECIFIC UPDATES
-    # Use the template below to define dataset and collection specific ontology changes. Will only apply to dataset
-    # if it matches a condition.
-    # If no such changes are needed, leave blank
-    # Examples:
-    # if dataset_id == "<dataset_1_id>":
-    #   <no further logic necessary>
-    #   utils.replace_ontology_term(df, <ontology_name>, {"term_to_replace": "replacement_term", ...})
-    # elif dataset_id == "<dataset_2_id>":
-    #   <custom transformation logic beyond scope of util functions>
-    # elif collection_id == "<collection_1_id>":
-    #   <no further logic necessary>
-    #   utils.replace_ontology_term(df, <ontology_name>, {"term_to_replace": "replacement_term", ...})
-    # elif collection_id == "<collection_2_id>":
-    #   <custom transformation logic beyond scope of replace_ontology_term>
-    # ...
 
-    if GENCODE_MAPPER:
-        dataset = utils.remap_deprecated_features(adata=dataset, remapped_features=GENCODE_MAPPER)
+    # https://github.com/chanzuckerberg/single-cell-curation/issues/1023
 
-    # AUTOMATED, DO NOT CHANGE -- IF GENCODE UPDATED, DEPRECATED FEATURE FILTERING ALGORITHM WILL GO HERE.
-    if DEPRECATED_FEATURE_IDS:
-        dataset = utils.remove_deprecated_features(adata=dataset, deprecated=DEPRECATED_FEATURE_IDS)
+    # Checking pre-identified datasets with non-csr matrices for sparsity
+    # If sparsity is high enough, converting to sparse_csr
+    if dataset_id in NON_CSR_DATASETS:
+        dataset = dataset.to_memory()
+        dataset = utils.check_non_csr_matrices(dataset)
+
+    # Public dataset changes
+    if collection_id == "33f48a52-31d8-4cc8-bd00-1e89c659a87f":
+        utils.replace_ontology_term(dataset.obs, "cell_type", {"CL:0000065": "CL:4052001"})
+        utils.replace_ontology_term(dataset.obs, "tissue", {"CL:0000065":"CL:4052001"})
+
+    if collection_id == "ef9f0fae-c12c-4f1a-bb67-63346c2ae0f1":
+        utils.map_ontology_term(dataset.obs,
+         "cell_type",
+         "author_cell_type", 
+         {
+            "Type IIX": "CL:4052026",
+            "NMJ":"CL:4052025",
+            "MRJ":"CL:4052024"
+         })
+
+    if collection_id == "964581c3-b5ac-494d-9bd4-0dcb6fb058da":
+        utils.map_ontology_term(dataset.obs,
+         "cell_type",
+         "author_cell_type", 
+         {
+            "Intercalated": "CL:4052048",
+            "Striated": "CL:4052049"
+         })
+
+    if collection_id == "350fe77e-cc64-482e-a42a-18bec9e4a629":
+        utils.map_ontology_term(dataset.obs, "cell_type", "cluster.names", {"Early Theca": "CL:4052010"})
+
+    if collection_id == "1e313b15-4aca-4e5a-94a3-1093c4c42abd":
+        utils.replace_ontology_term(dataset.obs, "assay", {"EFO:0008953":"EFO:0022845"})
+
+    # Private dataset changes
+
+    # Changes for dataset in private collection "Mouse Post-Flu Time Series"
+    if dataset.uns["title"] == "Mouse Post-Flu Time Series":
+        utils.replace_ontology_term(dataset.obs, "cell_type", {"CL:0002503":"CL:4052030"})
+
+    # Changes for datasets in private collection "HTAN CHOP - A longitudinal single-cell atlas of pediatric high-grade glioma."
+
+    if dataset.uns["title"] == "Longitudinal single-nucleus RNA-seq atlas of pediatric high-grade glioma":
+        utils.replace_ontology_term(dataset.obs, "disease", {"MONDO:0100342":"MONDO:1010030"})
+
+    if dataset.uns["title"] == "Longitudinal single-nucleus RNA-seq atlas of neoplastic cells in pediatric high-grade glioma":
+        utils.replace_ontology_term(dataset.obs, "disease", {"MONDO:0100342":"MONDO:1010030"})
+
+    if dataset.uns["title"] == "Longitudinal single-nucleus RNA-seq atlas of myeloid cells in pediatric high-grade glioma":
+        utils.replace_ontology_term(dataset.obs, "disease", {"MONDO:0100342":"MONDO:1010030"})
+
+    # Changes for datasets in private collection "Human Breast Cancer Single Cell Atlas"
+
+    if dataset.uns["title"] == "Global Atlas" and 'BC17086-12_Tumor' in dataset.obs['donor_id'].unique():
+        utils.map_ontology_term(dataset.obs, "development_stage", "donor_id", {"TBB330": "HsapDv:0000274"})
+
+    if dataset.uns["title"] == "Immune Compartment" and 'BC17086-12_Tumor' in dataset.obs['donor_id'].unique():
+        utils.map_ontology_term(dataset.obs, "development_stage", "donor_id", {"TBB330": "HsapDv:0000274"})
+
+    if dataset.uns["title"] == "Epithelial Compartment" and 'BC17086-12_Tumor' in dataset.obs['donor_id'].unique():
+        utils.map_ontology_term(dataset.obs, "development_stage", "donor_id", {"TBB330": "HsapDv:0000274"})
+
+    if dataset.uns["title"] == "Stromal Compartment" and 'wu_natgen_CID44041' in dataset.obs['donor_id'].unique():
+        utils.map_ontology_term(dataset.obs, "development_stage", "donor_id", {"TBB330": "HsapDv:0000274"})
+
+    #  Changes for dataset in private collection "Single cell RNA sequencing of the human embryonic meninges at 5-13 weeks post conception"
+
+    if dataset.uns["title"] == "All_cells" and 'XHU364' in dataset.obs['donor_id'].unique():
+        utils.replace_ontology_term(dataset.obs, "cell_type", {"CL:0011026":"CL:4042021"})
+
+    if dataset.uns["title"] == "Fibroblasts" and 'XHU364' in dataset.obs['donor_id'].unique():
+        utils.replace_ontology_term(dataset.obs, "cell_type", {"CL:0011026":"CL:4042021"})
+
+    if dataset.uns["title"] == "Erythropoietic" and 'XHU364' in dataset.obs['donor_id'].unique():
+        utils.replace_ontology_term(dataset.obs, "cell_type", {"CL:0011026":"CL:4042021"})
+
+    if dataset.uns["title"] == "Epithelial" and 'XDD400' in dataset.obs['donor_id'].unique():
+        utils.replace_ontology_term(dataset.obs, "cell_type", {"CL:0011026":"CL:4042021"})
+
+    if dataset.uns["title"] == "Neural" and 'XHU364' in dataset.obs['donor_id'].unique():
+        utils.replace_ontology_term(dataset.obs, "cell_type", {"CL:0011026":"CL:4042021"})
+
+    if dataset.uns["title"] == "Immune" and 'XHU364' in dataset.obs['donor_id'].unique():
+        utils.replace_ontology_term(dataset.obs, "cell_type", {"CL:0011026":"CL:4042021"})
+
+    if dataset.uns["title"] == "Vascular_endothelial" and 'XHU364' in dataset.obs['donor_id'].unique():
+        utils.replace_ontology_term(dataset.obs, "cell_type", {"CL:0011026":"CL:4042021"})
+
+    if dataset.uns["title"] == "Vascular_perivascular" and 'XHU364' in dataset.obs['donor_id'].unique():
+        utils.replace_ontology_term(dataset.obs, "cell_type", {"CL:0011026":"CL:4042021"})
+
+    if dataset.uns["title"] == "Neural_crest" and 'XHU364' in dataset.obs['donor_id'].unique():
+        utils.replace_ontology_term(dataset.obs, "cell_type", {"CL:0011026":"CL:4042021"})
 
     with dask.config.set(scheduler="single-threaded"):
         dataset.write_h5ad(output_file, compression="gzip")
