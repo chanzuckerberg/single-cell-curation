473500ee2f1846b45873f1320b757460c4765e32
Joyce Yan <5653616+joyceyan@users.noreply.github.com>
2025-09-09 17:00:35 -0400

    feat: cellosaurus terms for tissue_ontology_term_id (#1443)

--- cellxgene_schema_cli/tests/test_schema_compliance.py
+++ cellxgene_schema_cli/tests/test_schema_compliance.py
@@ -920,6 +920,7 @@ class TestObs:
     ):
         obs = validator_with_adata.adata.obs
         obs.loc[obs.index[0], "tissue_type"] = "cell line"
+        obs.loc[obs.index[0], "tissue_ontology_term_id"] = "CVCL_0001"
         obs.loc[obs.index[0], "development_stage_ontology_term_id"] = development_stage_ontology_term_id
         validator_with_adata.validate_adata()
         assert validator_with_adata.errors == errors
@@ -1057,6 +1058,7 @@ class TestObs:
         obs.at["Y", "tissue_type"] = "cell line"
         obs.at["Y", "cell_type_ontology_term_id"] = "na"
         obs.at["Y", "development_stage_ontology_term_id"] = "na"
+        obs.at["Y", "tissue_ontology_term_id"] = "CVCL_0001"
         assert validator.validate_adata()
         assert validator.errors == []
 
@@ -1109,6 +1111,33 @@ class TestObs:
         obs = validator.adata.obs
 
         obs.at["Y", "tissue_type"] = "organoid"
+        obs.at["Y", "tissue_ontology_term_id"] = "UBERON:0000922"
+
+        assert not validator.validate_adata()
+        assert len(validator.errors) > 0
+
+    def test_tissue_ontology_term_id__cell_line_valid(self, validator_with_adata):
+        """
+        Test 'CVCL_0001' tissue_ontology_term_id is valid if tissue_type is 'cell line'
+        """
+        validator = validator_with_adata
+        obs = validator.adata.obs
+
+        obs.at["Y", "tissue_type"] = "cell line"
+        obs.at["Y", "development_stage_ontology_term_id"] = "na"
+        obs.at["Y", "tissue_ontology_term_id"] = "CVCL_0001"
+
+        assert validator.validate_adata()
+        assert validator.errors == []
+
+    def test_tissue_ontology_term_id__cell_line_invalid(self, validator_with_adata):
+        """
+        Test 'UBERON:0000922' tissue_ontology_term_id is valid if tissue_type is 'cell line'
+        """
+        validator = validator_with_adata
+        obs = validator.adata.obs
+
+        obs.at["Y", "tissue_type"] = "cell line"
         obs.at["Y", "tissue_ontology_term_id"] = "UBERON:0000922"
 
         assert not validator.validate_adata()
--- cellxgene_schema_cli/tests/test_validate.py
+++ cellxgene_schema_cli/tests/test_validate.py
@@ -266,7 +266,12 @@ class TestAddLabelFunctions:
 
     def test_get_dictionary_mapping_curie__self_reported_ethnicity_ontology_term_id(self, schema_def, label_writer):
         ids = ["HANCESTRO:0005", "HANCESTRO:0014", "HANCESTRO:0005 || HANCESTRO:0014", "unknown"]
-        labels = ["European", "Hispanic or Latin American", "European || Hispanic or Latin American", "unknown"]
+        labels = [
+            "European ancestry",
+            "Latin or Admixed American ancestry",
+            "European ancestry || Latin or Admixed American ancestry",
+            "unknown",
+        ]
         curie_constraints = schema_def["components"]["obs"]["columns"]["self_reported_ethnicity_ontology_term_id"][
             "dependencies"
         ][0]["curie_constraints"]
