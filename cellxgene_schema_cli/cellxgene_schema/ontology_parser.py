5b05da28c17dabd80201b503a93a5c10a7a688df
Joyce Yan <5653616+joyceyan@users.noreply.github.com>
2025-09-09 12:23:07 -0400

    use alpha version in onto parser

--- cellxgene_schema_cli/cellxgene_schema/ontology_parser.py
+++ cellxgene_schema_cli/cellxgene_schema/ontology_parser.py
@@ -1,3 +1,3 @@
 from cellxgene_ontology_guide.ontology_parser import OntologyParser
 
-ONTOLOGY_PARSER = OntologyParser(schema_version="v7.0.0")
+ONTOLOGY_PARSER = OntologyParser(schema_version="v7.0.0-alpha")
